6f6826bf2d157ec646515420cc590592aafa746a
bobin13 <bobin13arsenal@gmail.com>
2023-12-14 10:28:00 -0500

    Delete components directory

--- components/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <nav>
-        <a href="index.html">Home</a>
-        <a href="FindTrip.html">Find Trip</a>
-        <a href="PostTrip.html">Post Trip</a>
-        <a href="page4.html">Inbox</a>
-    </nav>
-</header>
